cbcc3974dcf48a5fa1bfc18eaa5733d7b15f64e7
evlev777 <levkovich.evgeniy.98@gmail.com>
2023-04-17 18:08:53 +0300

    feat:complete task queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -15,19 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.last = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const newItem = new ListNode(value);
+    if (this.head === null) {
+      this.head = newItem;
+      return;
+    } else if (this.last === null) {
+      this.head.next = newItem;
+      this.last = newItem;
+      return;
+    }
+
+    this.last.next = newItem;
+    this.last = newItem;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const previousHeadValue = this.head.value;
+    this.head = this.head.next;
+    return previousHeadValue;
   }
 }
 
